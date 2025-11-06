d5b8deab8cde53993b6b98187ba543d1916979f6
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-07 00:09:34 +0530

    variables

---
+++ day01/coding-practice/variables.js
@@ -0,0 +1,6 @@
+// variables are  containers for storing data.
+let name = 'Shubhamm';
+console.log(name)
+// JavaScript dynamically typed - variables types can change at anytime.
+let data = 5;
+data = "Now I'm a string!";
