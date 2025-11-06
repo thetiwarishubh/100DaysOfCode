773aaee26ace3ad6cf5d5d011b3477848242f5fb
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-07 00:14:12 +0530

    data types

---
+++ day01/coding-practice/datatypes.js
@@ -0,0 +1,16 @@
+// Data types tell JavaScript what kind of value a variable holds.
+
+// Two main categoris of data types 
+// 1. Primitve Data types 
+let name = "Riya"; // string
+let age = 21; //number
+let isOnline = false; //boolean
+let score = null; //object
+let count; //undefined
+let id = Symbol("userID");  //symbol
+let bigNum = 999999999999999999999999999999n; //bigInt
+
+//2. Non-primitve Data types
+// Obejct 
+//Array
+//Function
