bb456f14a07e9e04593b861ee57d83a6d3b9f4ae
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-08 23:58:12 +0530

    operators in js

---
+++ day01/coding-practice/operators.js
@@ -0,0 +1,10 @@
+let score = 16;
+let scoreTwo = 4;
+console.log(score + scoreTwo)
+console.log(score - scoreTwo)
+console.log(score * scoreTwo)
+console.log(score / scoreTwo)
+console.log(score % scoreTwo)
+console.log(score ** scoreTwo)
+console.log(++score)
+console.log(--score)
