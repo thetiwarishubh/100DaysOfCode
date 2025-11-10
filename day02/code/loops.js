3d806d781dbd384091817341c97dc5c4265430fa
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-10 23:25:59 +0530

    loops starting

---
+++ day02/code/loops.js
@@ -0,0 +1,17 @@
+// Loops can excute a block of code a number of time;
+
+//Print "Hello World!" 10 Times;
+
+for(let i = 0; i < 10; i++){
+    console.log(`${i + 1}: Hello World!`)
+}
+
+// for(exp1, exp2, exp3){
+    // This is code 
+// }
+
+//Exp1 is excuted (one time) before the execution of the code block.
+// exp2 is define the conditions for executing the code block
+// exp3 is executed (every time) after the code block has been executed.
+
+// Pritn
