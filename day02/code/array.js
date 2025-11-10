1dd4ec27157e35a1204bd57ffd268ff3d81d03e8
Shubham Tiwarigit config --global user.email Iamtiwarisam@gmail.com <iamtiwarisam@gmail.com>
2025-11-11 02:03:51 +0530

    array definition

---
+++ day02/code/array.js
@@ -0,0 +1,9 @@
+//  An array is an object type designed for storing data collections.
+/* Key characteristics of javascript arrays are:- 
+Element :- An Array is a list of values, known as elements.
+Ordered :- Array elements are ordered based on their index.
+Zero indexed :- The first is at index 0, the second at index 1, and so on,
+Dynamic size :- Array can grow or shrink as elements are added ir removed.
+Heterogeneous :- Arrays can store elements of differnt data types(numbers, string, null, undefined, boolean, bigInt, symbol, object and other array.)
+ */ 
+
